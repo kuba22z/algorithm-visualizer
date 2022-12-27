93f9cc103c4314ee693587341ef3cdc365ba2364
kuba22z <your@email.com>
2022-12-27 12:49:53 +0100

    try fix api 405 error

--- my-web-app/next.config.js
+++ my-web-app/next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
     images: {unoptimized: true},
     reactStrictMode: true,
     swcMinify: true,
+    async rewrites() {
+        return [
+            {
+                source: '/dashboard',
+                destination: 'https://kuba22z.github.io/api/bbSort',
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
