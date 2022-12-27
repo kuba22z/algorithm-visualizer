7c520eb6bdd81c90a6376a68b2684fea30768a8b
kuba22z <your@email.com>
2022-12-27 23:54:31 +0100

    add api generator codegen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,17 @@
 import Typography from "@mui/material/Typography";
 import * as React from "react";
-import {gql, useQuery} from '@apollo/client';
+import {Book, useGetBooksQuery} from "../generated/graphql";
 
 export default function Home() {
-    const GET_LOCATIONS = gql`
-        query GetBook{
-            books {
-                title,author
-            }
-        }
-    `;
-    const {loading, error, data} = useQuery(GET_LOCATIONS);
-
+    const {loading, error, data} = useGetBooksQuery();
     const err = () => {
         if (error) return <p>Error : {error.message}</p>;
         else if (loading) return <p>Loading...</p>;
-        else return data?.books.map((book: { title: string, author: string }) => {
-                return <p key={book.title}>
-                    {"book Title" + book.title + " author " + book.author}
-                </p>
-            })
+        else if (data) return data.books.map((book: Book) => {
+            return <p key={book.title}>
+                {"book Title " + book.title + " author " + book.author}
+            </p>
+        })
     }
     return (
         <div>
