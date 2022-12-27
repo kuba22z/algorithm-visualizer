57e794a8c33d1fe11ac5ef04985c06820fd99bcd
kuba22z <your@email.com>
2022-12-27 13:33:01 +0100

    try fix api 405 error

--- my-web-app/next.config.js
+++ my-web-app/next.config.js
@@ -4,12 +4,21 @@ const nextConfig = {
     images: {unoptimized: true},
     reactStrictMode: true,
     swcMinify: true,
-    async rewrites() {
+    async headers() {
         return [
             {
-                source: '/api/:path*',
-                destination: 'https://kuba22z.github.io/api/:path*',
-            },
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    {key: "Access-Control-Allow-Credentials", value: "true"},
+                    {key: "Access-Control-Allow-Origin", value: "https://kuba22z.github.io"},
+                    {key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT"},
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+                    },
+                ]
+            }
         ]
     },
 }
